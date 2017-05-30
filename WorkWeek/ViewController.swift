15b487785c46a153db694f9f1c559f46b2db32e0
John Regner <regnerjr@gmail.com>
2017-05-30 07:22:49 -0700

    Remove boilerplate code

--- WorkWeek/ViewController.swift
+++ WorkWeek/ViewController.swift
@@ -1,8 +1,4 @@
 //
-//  ViewController.swift
-//  WorkWeek
-//
-//  Created by John Regner on 5/29/17.
 //  Copyright © 2017 Spark App Studio. All rights reserved.
 //
 
@@ -10,17 +6,6 @@ import UIKit
 import RealmSwift
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
