797574f78bb095712885d7dec6dbab6079d4dae7
John Regner <regnerjr@gmail.com>
2017-06-01 22:01:18 -0700

    Add Coordinator.

---
+++ WorkWeek/AppCoordinator.swift
@@ -0,0 +1,19 @@
+//
+//  Copyright © 2017 Spark App Studio. All rights reserved.
+//
+
+import UIKit
+
+class AppCoordinator {
+
+    let navigationController: UINavigationController
+
+    init(with navController: UINavigationController) {
+        self.navigationController = navController
+    }
+
+    func start() {
+
+    }
+
+}
