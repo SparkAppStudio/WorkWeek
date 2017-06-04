c0b11855f948387cc6ac386811f35eaf1c8c5ddd
John Regner <regnerjr@gmail.com>
2017-06-04 09:10:10 -0700

    Add initial cut of settings picker
    With separate picker data source object.

--- WorkWeek/SettingsViewController.swift
+++ WorkWeek/SettingsViewController.swift
@@ -24,17 +24,21 @@ final class SettingsViewController: UIViewController {
     @IBOutlet weak var saturday: UIButton!
     @IBOutlet weak var sunday: UIButton!
 
+    @IBOutlet weak var picker: UIPickerView!
+    var pickerDataSource = WorkDayHoursPickerDataSource()
+
     // MARK: View Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         setMainContentStackViewEqualToPhoneWidth()
         configureStyle(of: work, home)
         configureStyle(of: monday, tuesday, wednesday, thursday, friday, saturday, sunday)
 
-        guard let monday = monday else {
-            assertionFailure("There should always be a monday")
-        }
+        picker.delegate = pickerDataSource
+        picker.dataSource = pickerDataSource
+        pickerDataSource.delegate = self
     }
 
     override var prefersStatusBarHidden: Bool {
@@ -53,6 +57,49 @@ final class SettingsViewController: UIViewController {
         }
     }
 
+}
+
+extension SettingsViewController: PickerResponseForwarder {
+    func didSelectWork(hours: Double) {
+        print("Selected \(hours)")
+    }
+}
+
+protocol PickerResponseForwarder: class {
+    func didSelectWork(hours: Double)
+}
+
+class WorkDayHoursPickerDataSource: NSObject, UIPickerViewDataSource, UIPickerViewDelegate {
+
+    weak var delegate: PickerResponseForwarder?
+
+    let pickerData: [Double] = {
+        let str = stride(from: 0.5, to: 24, by: 0.5)
+        return Array(str)
+    }()
+
+    // This picker is simple, it just show one list of numbers, 0-24 in half hour increments.
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    // There will only ever be one component so....
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        //0.5 for a 30 min work day
+        // then each half hour till 23.5?
+        // 1, 1.5, 2, 2.5, 3, 3.5,....23, 23.5 = 23*2 = 46 plus the initial 30 min one.
+        return pickerData.count
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        // 50, 100, 150, 200,
+        let num = pickerData[row]
+        return "\(num)"
+    }
+
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        delegate?.didSelectWork(hours: pickerData[row])
+    }
 }
 
 extension SettingsViewController: StoryboardSceneBased {
