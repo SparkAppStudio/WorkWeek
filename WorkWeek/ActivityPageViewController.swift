047c2aa7921c554f3d5fbb292265f141ff39ccba
Jay Yupin Hu <jay@projectyx.com>
2017-06-11 20:44:36 -0700

    using new shinny datasource for pageviewcontroller

--- WorkWeek/ActivityPageViewController.swift
+++ WorkWeek/ActivityPageViewController.swift
@@ -5,70 +5,26 @@
 import UIKit
 import Reusable
 
-class ActivityPageViewController: UIPageViewController {
-    fileprivate(set) lazy var orderedViewControllers: [UIViewController] = {
-        return [self.newViewController("CountdownViewController"),
-                self.newViewController("DailyTableViewController"),
-                self.newViewController("WeeklyTableViewController")]
+class ActivityPageViewController: UIPageViewController, ActivityStoryboard {
+
+    var orderedViewControllers = [CountdownViewController.instantiate(),
+                                  DailyTableViewController.instantiate(),
+                                  WeeklyTableViewController.instantiate()]
+
+    lazy var manager: PageManager = {
+        return PageManager(types: self.orderedViewControllers)
     }()
-    fileprivate func newViewController(_ vcName: String) -> UIViewController {
-        return UIStoryboard(name: "Activity", bundle: nil) .
-            instantiateViewController(withIdentifier: vcName)
-    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.dataSource = self
-        // Loading up the first page
-        if let firstViewController = orderedViewControllers.first {
-            setViewControllers([firstViewController],
-                               direction: .forward,
-                               animated: true,
-                               completion: nil)
+
+        delegate = manager
+        dataSource = manager
+        guard let firstVC = orderedViewControllers.first else {
+            assertionFailure("No pages in array")
+            return
         }
+        setViewControllers([firstVC], direction: .forward, animated: false, completion: nil)
     }
 
-}
-extension ActivityPageViewController: UIPageViewControllerDataSource {
-    func pageViewController(_ pageViewController: UIPageViewController,
-                            viewControllerBefore viewController: UIViewController) -> UIViewController? {
-        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
-            return nil
-        }
-        let previousIndex = viewControllerIndex - 1
-        guard previousIndex >= 0 else {
-            return nil
-        }
-        guard orderedViewControllers.count > previousIndex else {
-            return nil
-        }
-        return orderedViewControllers[previousIndex]
-    }
-    func pageViewController(_ pageViewController: UIPageViewController,
-                            viewControllerAfter viewController: UIViewController) -> UIViewController? {
-        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
-            return nil
-        }
-        let nextIndex = viewControllerIndex + 1
-        let orderedViewControllersCount = orderedViewControllers.count
-        guard orderedViewControllersCount != nextIndex else {
-            return nil
-        }
-        guard orderedViewControllersCount > nextIndex else {
-            return nil
-        }
-        return orderedViewControllers[nextIndex]
-    }
-    func presentationCount(for pageViewController: UIPageViewController) -> Int {
-        return orderedViewControllers.count
-    }
-    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
-        guard let firstViewController = viewControllers?.first,
-            let firstViewControllerIndex = orderedViewControllers.index(of: firstViewController) else {
-                return 0
-        }
-        return firstViewControllerIndex
-    }
-}
-extension ActivityPageViewController: StoryboardSceneBased {
-    static let sceneStoryboard = UIStoryboard(name: "Activity", bundle: nil)
 }
