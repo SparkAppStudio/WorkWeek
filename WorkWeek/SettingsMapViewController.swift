6b3a3dcf9aff42df932245fe112d48359148850f
John Regner <regnerjr@gmail.com>
2017-06-29 21:33:36 -0700

    Add drawing of any existing regions

--- WorkWeek/SettingsMapViewController.swift
+++ WorkWeek/SettingsMapViewController.swift
@@ -10,6 +10,11 @@ enum MapVCType {
     case work
 }
 
+enum RegionId: String {
+    case home
+    case work
+}
+
 protocol MapVCDelegate: class {
     func save(type: MapVCType, coordinate: CLLocationCoordinate2D, radius: CLLocationDistance)
     func cancel()
@@ -42,6 +47,8 @@ class SettingsMapViewController: UIViewController, SettingsStoryboard {
             headerLabel.text = NSLocalizedString("Work", comment: "Settings Map Set Work Location")
         }
 
+        drawOverlays(for: type)
+
         // TODO : Remove this, we should already have authorization when we get here
         CLLocationManager().requestAlwaysAuthorization()
     }
@@ -61,6 +68,39 @@ class SettingsMapViewController: UIViewController, SettingsStoryboard {
     @IBAction func didTapCancel(_ sender: UIButton) {
         delegate?.cancel()
     }
+
+    func drawOverlays(for type: MapVCType) {
+        switch type {
+        case .home:
+            let manager = CLLocationManager()
+            let allRegions = manager.monitoredRegions
+            let homeRegions = allRegions.filter { $0.identifier == RegionId.home.rawValue }
+            guard let firstHomeRegion = homeRegions.first else {
+                Log.log("No \(type) Region Currently Registed, only \(allRegions)")
+                return
+            }
+            guard let homeCircle = firstHomeRegion as? CLCircularRegion else {
+                Log.log("Found \(firstHomeRegion), but its not a CLCircularRegion")
+                return
+            }
+            let home = MKCircle(center: homeCircle.center, radius: homeCircle.radius)
+            mapView.add(home)
+        case .work:
+            let manager = CLLocationManager()
+            let allRegions = manager.monitoredRegions
+            let homeRegions = allRegions.filter { $0.identifier == RegionId.work.rawValue }
+            guard let firstWorkRegion = homeRegions.first else {
+                Log.log("No \(type) Region Currently Registed, only \(allRegions)")
+                return
+            }
+            guard let workCircle = firstWorkRegion as? CLCircularRegion else {
+                Log.log("Found \(firstWorkRegion), but its not a CLCircularRegion")
+                return
+            }
+            let work = MKCircle(center: workCircle.center, radius: workCircle.radius)
+            mapView.add(work)
+        }
+    }
 }
 
 extension SettingsMapViewController: MKMapViewDelegate {
@@ -75,6 +115,17 @@ extension SettingsMapViewController: MKMapViewDelegate {
             mapView.setRegion(userZoomedRegion, animated: true)
         }
     }
+
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        print(annotation)
+        return nil
+    }
+
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        print(overlay)
+        return MKCircleRenderer(overlay: overlay)
+    }
+
 }
 
 class CenterCircleView: UIView {
