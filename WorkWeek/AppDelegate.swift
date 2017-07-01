281b3fbd4748ed2b8c1adfb6faa29799998cebb6
John Regner <regnerjr@gmail.com>
2017-07-01 16:47:27 -0700

    Create Location Manager to live in app delegate

--- WorkWeek/AppDelegate.swift
+++ WorkWeek/AppDelegate.swift
@@ -3,16 +3,19 @@
 //
 
 import UIKit
+import CoreLocation
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
     var appCoordinator: AppCoordinator!
+    var locationManager: CLLocationManager!
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+
+        createLocationManager()
 
         CrashReporting.configure()
 
@@ -31,6 +34,46 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         appCoordinator.start()
 
         window?.makeKeyAndVisible()
+    }
+
+    func createLocationManager() {
+        let locationManager = CLLocationManager()
+        locationManager.delegate = self
+        self.locationManager = locationManager
+    }
+}
+
+
+extension AppDelegate: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        if (error as? CLError)?.code == .denied {
+            manager.stopUpdatingLocation()
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        guard let typedRegion = RegionId(rawValue: region.identifier) else {
+            Log.log(.error, "Could not create a typed region from \(region.identifier)")
+            return
+        }
+        switch typedRegion {
+        case .home:
+            Log.log("Arrived Home")
+        case .work:
+            Log.log("Arrived Work")
+        }
+    }
+    func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
+        guard let typedRegion = RegionId(rawValue:region.identifier) else {
+            Log.log(.error, "Could not create a typed region from \(region.identifier)")
+            return
+        }
+        switch typedRegion {
+        case .home:
+            Log.log("Leaving Home")
+        case .work:
+            Log.log("Leaving Work")
+        }
     }
 
 }
