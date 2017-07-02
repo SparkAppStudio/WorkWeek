0d0450ff39ed4dd9b078e15ef664e1a41be1e64d
Douglas Hewitt <doughewitt4@gmail.com>
2017-07-01 17:34:35 -0700

    insert notify onboarding screen and fix debounce
    after they accept location, add the notify request VC to the page view
    controller
    
    fix the debounce by removing it and extending the pages widths instead
    to hide the black background

--- WorkWeek/PageManager.swift
+++ WorkWeek/PageManager.swift
@@ -15,8 +15,6 @@ class PageManager: NSObject, UIPageViewControllerDelegate, UIPageViewControllerD
     func pageViewController(_ pageViewController: UIPageViewController,
                             viewControllerBefore viewController: UIViewController) -> UIViewController? {
 
-        preventBounce(pageViewController: pageViewController)
-
         guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
             return nil
         }
@@ -32,8 +30,6 @@ class PageManager: NSObject, UIPageViewControllerDelegate, UIPageViewControllerD
 
     func pageViewController(_ pageViewController: UIPageViewController,
                             viewControllerAfter viewController: UIViewController) -> UIViewController? {
-
-        preventBounce(pageViewController: pageViewController)
 
         guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
             return nil
@@ -59,19 +55,5 @@ class PageManager: NSObject, UIPageViewControllerDelegate, UIPageViewControllerD
                 return 0
         }
         return currentIndex
-    }
-
-    // Used to Prevent user from swiping toward the edge and seeing black background behind.
-    // This is done for every page, 
-    // as black background can be seen between if user swipes aggressively
-    // TODO: This doesn't actually work, however moving it to the pageVC itself breaks scrolling altogether
-    func preventBounce(pageViewController: UIPageViewController) {
-        // Iterate through the views looking for scroll view
-        // (which is the view that controls the pages swiping and bounce)
-        // and disable bounce
-        for subview in pageViewController.view.subviews where subview is UIScrollView {
-            guard let view = subview as? UIScrollView else {break}
-            view.bounces = false
-        }
     }
 }
