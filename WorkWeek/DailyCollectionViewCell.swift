65171a1c763b2509436c5fe7f39f2ce55a01e00e
John Regner <regnerjr@gmail.com>
2017-07-11 10:49:58 -0700

    More Keypath

--- WorkWeek/DailyCollectionViewCell.swift
+++ WorkWeek/DailyCollectionViewCell.swift
@@ -14,8 +14,13 @@ class DailyCollectionViewCell: UICollectionViewCell, Reusable {
     @IBOutlet weak var eventTimeLabel: UILabel!
 
     func configureCell(_ event: Event) {
-        //TODO: Do you know why this is not a "TypeSafe" property access?
-        guard let activityNameString = event.value(forKey: "eventName") as? String else {
+        // TODO: Do you know why this is not a "TypeSafe" property access?
+        // TODO: Update this code to not be so "Stringy"
+        // i.e. We're getting a string from the event, then switching over it, to match 
+        // an enums raw value then returning a string.
+        // Perhaps Event should know what type of Notification Created it...
+        // then we could switch over that EventType, to find the correct display String. 
+        guard let activityNameString = event.value(forKey: #keyPath(Event.eventName)) as? String else {
             return
         }
         let cases = NotificationCenter.CheckInEvent.self
@@ -32,7 +37,7 @@ class DailyCollectionViewCell: UICollectionViewCell, Reusable {
             eventNameLabel.text = ""
         }
 
-        guard let activityTimeDate = event.value(forKey: "eventTime") as? Date else {
+        guard let activityTimeDate = event.value(forKey: #keyPath(Event.eventTime)) as? Date else {
             return
         }
 
