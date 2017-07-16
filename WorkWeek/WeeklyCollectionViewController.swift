10dd1f01e9b0d3f09c4d4101ed06ff7082127970
Jay Yupin Hu <yhu98@ucsc.edu>
2017-07-15 17:27:43 -0700

    wip

--- WorkWeek/WeeklyCollectionViewController.swift
+++ WorkWeek/WeeklyCollectionViewController.swift
@@ -4,15 +4,59 @@
 
 import UIKit
 import Reusable
+import RealmSwift
 
 class WeeklyCollectionViewController: UICollectionViewController {
+
+    lazy var realm: Realm? = {
+        do {
+            var realm = try Realm()
+            return realm
+        } catch {
+            Log.log("error access realm")
+            return nil
+        }
+    }()
+
+    lazy var results: Results<WeeklyObject>? = {
+        do {
+            let now = Date()
+            var results = try Realm().objects(WeeklyObject.self)
+            return results
+        } catch {
+            Log.log("error querying daily object")
+            return nil
+        }
+    }()
+
+    var notificationToken: NotificationToken?
 
     var weeklyReports = [WeeklyObject]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupRealm()
     }
 
+    func setupRealm() {
+        self.notificationToken = results?.addNotificationBlock({ (changes: RealmCollectionChange) in
+            switch changes {
+            case .initial:
+                self.collectionView?.reloadData()
+                break
+            case .update(_, deletions: let deletions, insertions: let insertions, modifications: let modifications):
+                self.collectionView?.performBatchUpdates({
+                    self.collectionView?.insertItems(at: insertions.map {IndexPath(item: $0, section: 0)})
+                    self.collectionView?.deleteItems(at: deletions.map {IndexPath(item: $0, section: 0)})
+                    self.collectionView?.reloadItems(at: modifications.map {IndexPath(item: $0, section: 0)})
+                }, completion: nil)
+                break
+            case .error(let err):
+                Log.log("fatel error \(err)")
+                break
+            }
+        })
+    }
 }
 
 // DataSource
@@ -20,15 +64,19 @@ extension WeeklyCollectionViewController {
 
     override func collectionView(_ collectionView: UICollectionView,
                                  numberOfItemsInSection section: Int) -> Int {
-        weeklyReports = RealmManager.shared.queryAllObjects(ofType: WeeklyObject.self)
-        return weeklyReports.count
+//        weeklyReports = RealmManager.shared.queryAllObjects(ofType: WeeklyObject.self)
+//        return weeklyReports.count
+        return results?.count ?? 0
     }
 
     override func collectionView(_ collectionView: UICollectionView,
                                  cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
 
         let cell = collectionView.dequeueReusableCell(for: indexPath) as WeeklyCollectionViewCell
-        cell.configureCell(for: weeklyReports[indexPath.row])
+//        cell.configureCell(for: weeklyReports[indexPath.row])
+        if let weeklyObject = results?[indexPath.row] {
+            cell.configureCell(for: weeklyObject)
+        }
         return cell
     }
 
