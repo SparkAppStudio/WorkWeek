9414601038a3c109c6db07aa3d7c49a0a87facfb
John Regner <regnerjr@gmail.com>
2017-08-14 20:47:22 -0700

    lots of cleanup work.
    Cleaned up the Button with BG and shadow, and added new gradient button.
    
    Still need to figure out how to mask the gradient, and add some text to
    the buttons... maybe the text should just be a label....

--- WorkWeekCustomViews.playground/Pages/ButtonWithBGandShadow.xcplaygroundpage/Contents.swift
+++ WorkWeekCustomViews.playground/Pages/ButtonWithBGandShadow.xcplaygroundpage/Contents.swift
@@ -1,46 +1,12 @@
 //: [Previous](@previous)
 
 import UIKit
-
-class MyVC: UIViewController {
-    @objc func buttonTapped(_ sender: UIButton) {
-        print("Button was tapped")
-    }
-}
-
-class GradientLayer: CALayer {
-
-    var color1: UIColor
-    var color2: UIColor
-
-    init(color1: UIColor, color2: UIColor) {
-        self.color1 = color1
-        self.color2 = color2
-        super.init()
-    }
-    required init?(coder aDecoder: NSCoder) {
-        fatalError()
-    }
-
-    override func draw(in ctx: CGContext) {
-        ctx.setFillColor(UIColor.red.cgColor)
-        ctx.fill(self.frame)
-    }
-
-    var gradient: CGGradient {
-        let colorSpace = CGColorSpaceCreateDeviceRGB()
-        let colors = color1.colorComponents + color2.colorComponents
-        var locations: [CGFloat] = [0.0, 0.5]
-        let g = CGGradient(colorSpace: colorSpace, colorComponents: colors, locations: &locations, count: locations.count)!
-        return g
-    }
-
-}
+import PlaygroundSupport
 
 let mainBlue = UIColor(red:0.24, green:0.51, blue:0.97, alpha:1)
 let mainGreen = UIColor(red:0.21, green:0.93, blue:0.31, alpha:1)
 
-class GradientView: UIView {
+class ButtonWithBGandShadow: UIButton {
 
     override func draw(_ rect: CGRect) {
         guard let ctx = UIGraphicsGetCurrentContext() else {fatalError()}
@@ -74,19 +40,16 @@ class GradientView: UIView {
         ctx.closePath()
 
         ctx.setLineWidth(5)
-        let shadowSize = CGSize(width: 10, height: 10)
+        let shadowSize = CGSize(width: 5, height: 5)
         ctx.setShadow(offset: shadowSize, blur: 20)
 
-
-        ctx.setStrokeColor(UIColor.red.cgColor)
         ctx.setFillColor(mainBlue.cgColor)
         ctx.fillPath()
-        ctx.strokePath()
     }
 
     func drawBackground(_ ctx: CGContext, _ rect: CGRect) {
         var rect = rect
-        rect.origin = CGPoint(x: rect.origin.x, y: rect.origin.y - 2)
+        rect.origin = CGPoint(x: rect.origin.x, y: rect.origin.y - 4)
 
         let topMiddle = CGPoint(x: rect.midX, y: rect.minY)
         ctx.move(to: topMiddle)
@@ -103,25 +66,22 @@ class GradientView: UIView {
 
         ctx.closePath()
 
-        ctx.setFillColor(UIColor.red.cgColor)
+        ctx.setFillColor(mainGreen.cgColor)
         ctx.fillPath()
-    }
-
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-    }
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
     }
 }
 
+class Responder: NSObject {
+    @objc func buttonTapped(_ sender: UIButton) {
+        print("Button was tapped")
+    }
+}
 
-import PlaygroundSupport
+let responder = Responder()
 
-//let button = Button(frame: CGRect(x: 0, y: 0, width: 288, height: 75))
-//
-//button.target(forAction: #selector(MyVC.buttonTapped(_:)), withSender: nil)
+let button = ButtonWithBGandShadow(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+button.addTarget(responder, action: #selector(Responder.buttonTapped(_:)), for: .touchUpInside)
 
-PlaygroundPage.current.liveView = GradientView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+PlaygroundPage.current.liveView = button
 
 //: [Next](@next)
