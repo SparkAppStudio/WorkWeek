97ce60584196452ac9d64b16d56c01c8046f5750
Douglas Hewitt <doughewitt4@gmail.com>
2017-08-21 21:46:01 -0700

    updated app icon, launch screen, and onboarding text

---
+++ WorkWeek/Design.swift
@@ -0,0 +1,39 @@
+//
+//  Design.swift
+//  WorkWeek
+//
+//  Created by Douglas Hewitt on 8/21/17.
+//  Copyright © 2017 Spark App Studio. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static func workBlue() -> UIColor {
+        return UIColor(displayP3Red: 60/255, green: 130/255, blue: 248/255, alpha: 1)
+    }
+
+    static func homeGreen() -> UIColor {
+        return UIColor(displayP3Red: 54/255, green: 236/255, blue: 80/255, alpha: 1)
+    }
+
+    static func darkBackground() -> UIColor {
+        return UIColor(displayP3Red: 54/255, green: 52/255, blue: 57/255, alpha: 1)
+    }
+
+    static func darkContent() -> UIColor {
+        return UIColor(displayP3Red: 69/255, green: 68/255, blue: 68/255, alpha: 1)
+    }
+
+    static func lightBackground() -> UIColor {
+        return UIColor(displayP3Red: 231/255, green: 230/255, blue: 232/255, alpha: 1)
+    }
+
+    static func lightContent() -> UIColor {
+        return UIColor(displayP3Red: 240/255, green: 239/255, blue: 236/255, alpha: 1)
+    }
+
+    static func darkGrayText() -> UIColor {
+        return UIColor(displayP3Red: 74/255, green: 74/255, blue: 74/255, alpha: 1)
+    }
+}
