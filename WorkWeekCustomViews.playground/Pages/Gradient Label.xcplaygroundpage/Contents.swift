f7b096741328ee6c3f420576b99b41c34e99c504
John Regner <regnerjr@gmail.com>
2017-08-24 21:41:32 -0700

    Initial working version of gradient label...
    I think we'll want a proper subclass of label, where we can change the
    colors as part of the API, probably from an @IBInspectable

--- WorkWeekCustomViews.playground/Pages/Gradient Label.xcplaygroundpage/Contents.swift
+++ WorkWeekCustomViews.playground/Pages/Gradient Label.xcplaygroundpage/Contents.swift
@@ -3,18 +3,51 @@
 import UIKit
 
 class GradientLabel: UILabel {
-
 }
+
+func gradientImage(ofSize size: CGSize) -> UIImage? {
+    UIGraphicsBeginImageContext(size)
+    defer { UIGraphicsEndImageContext() }
+
+    let rect = CGRect(origin: CGPoint(x: 0, y: 0), size: size)
+
+    guard let ctx = UIGraphicsGetCurrentContext() else {
+        print("Sorry No context")
+        return nil
+    }
+
+    let colorSpace = CGColorSpaceCreateDeviceRGB()
+    let colors: NSArray = [UIColor.red.cgColor,
+                           UIColor.blue.cgColor]
+    guard let gradient = CGGradient(colorsSpace: colorSpace,
+                              colors: colors,
+                              locations: [0, 1]) else { return nil }
+    let startPoint = CGPoint(x: 0, y: 0)
+    let endPoint = CGPoint(x: 0, y: rect.size.height)
+
+    ctx.drawLinearGradient(gradient, start: startPoint, end: endPoint, options: [])
+    return UIGraphicsGetImageFromCurrentImageContext()
+}
+
 
 import PlaygroundSupport
 
 let label = GradientLabel(frame: CGRect(x: 0, y: 0, width: 150, height: 50))
-label.text = "5:38"
-label.textColor = UIColor.white
 label.font = UIFont.systemFont(ofSize: 58)
-label.backgroundColor = UIColor(white: 0.2, alpha: 1.0)
+label.text = "5:38"
 
-PlaygroundPage.current.liveView = label
+let size = label.sizeThatFits(CGSize(width: 100, height: 100))
+let gradientImg = gradientImage(ofSize: size)!
+label.textColor = UIColor(patternImage: gradientImg)
+
+
+
+let superView = UIView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+superView.backgroundColor = .white
+superView.addSubview(label)
+
+
+PlaygroundPage.current.liveView = superView
 
 
 //: [Next](@next)
