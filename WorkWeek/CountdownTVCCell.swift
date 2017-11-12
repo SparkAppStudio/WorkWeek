19cb30ee2648002f97aec4199e5dd24eceba1436
Jay Yupin Hu <jay.yupinhu@gmail.com>
2017-11-11 17:03:07 -0800

    progress stripe view takes a percentage now

---
+++ WorkWeek/CountdownTVCCell.swift
@@ -0,0 +1,32 @@
+//
+// Copyright © 2017 Spark App Studio All rights reserved.
+//
+
+import UIKit
+
+class CountdownTVCCell: UITableViewCell {
+
+
+    // MARK: - IBs
+
+    @IBOutlet weak var timeFrameLabel: UILabel!
+    @IBOutlet weak var hoursLabel: UILabel!
+    @IBOutlet weak var sundayView: ProgressStripeView!
+    @IBOutlet weak var mondayView: ProgressStripeView!
+    @IBOutlet weak var tuesdayView: ProgressStripeView!
+    @IBOutlet weak var wednesdayView: ProgressStripeView!
+    @IBOutlet weak var thursdayView: ProgressStripeView!
+    @IBOutlet weak var fridayView: ProgressStripeView!
+    @IBOutlet weak var saturdayView: ProgressStripeView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    func configure(_ weeklyObject: WeeklyObject) {
+        for (index, day) in weeklyObject.dailyObjects.enumerated() {
+            // given a data, i need to figure out days
+        }
+        mondayView.percentage = 0.9
+    }
+}
