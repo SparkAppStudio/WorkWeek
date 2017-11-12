badc245bf81e2485e92e76aa27ddb1412adb4eeb
Jay Yupin Hu <jay.yupinhu@gmail.com>
2017-11-12 12:27:33 -0800

    weekly graph view model

--- WorkWeek/WeeklyGraphCell.swift
+++ WorkWeek/WeeklyGraphCell.swift
@@ -20,21 +20,18 @@ class WeeklyGraphCell: UITableViewCell {
     @IBOutlet weak var fridayView: ProgressStripeView!
     @IBOutlet weak var saturdayView: ProgressStripeView!
 
-    func configure(_ weekObject: WeeklyObject) {
-
-        setupUI(weekObject.weekDaysWorkingPercentage)
-    }
-
-    func setupUI(_ weekDaysWorkingPercentage: WeekDaysWorkingPercent) {
+    func configure(_ weeklyGraphViewModel: WeeklyGraphViewModel) {
         setCornerRadius()
         setShadow()
-        sundayView.percentage = weekDaysWorkingPercentage.sundayPercent
-        mondayView.percentage = weekDaysWorkingPercentage.mondayPercent
-        tuesdayView.percentage = weekDaysWorkingPercentage.tuesdayPercent
-        wednesdayView.percentage = weekDaysWorkingPercentage.wednesdayPercent
-        thursdayView.percentage = weekDaysWorkingPercentage.thursdayPercent
-        fridayView.percentage = weekDaysWorkingPercentage.fridayPercent
-        saturdayView.percentage = weekDaysWorkingPercentage.saturdayPercent
+        sundayView.percentage = weeklyGraphViewModel.sundayPercent
+        mondayView.percentage = weeklyGraphViewModel.mondayPercent
+        tuesdayView.percentage = weeklyGraphViewModel.tuesdayPercent
+        wednesdayView.percentage = weeklyGraphViewModel.wednesdayPercent
+        thursdayView.percentage = weeklyGraphViewModel.thursdayPercent
+        fridayView.percentage = weeklyGraphViewModel.fridayPercent
+        saturdayView.percentage = weeklyGraphViewModel.saturdayPercent
+        timeFrameLabel.text = weeklyGraphViewModel.timeFrameText
+        hoursLabel.text = weeklyGraphViewModel.hoursText
     }
 
     private func setCornerRadius() {
@@ -49,5 +46,45 @@ class WeeklyGraphCell: UITableViewCell {
         shadownView.layer.shadowColor = UIColor.black.cgColor
         shadownView.layer.shadowOffset = CGSize(width: 0, height: 2)
     }
+}
+
+class WeeklyGraphViewModel {
+
+    var sundayPercent: Double = 0
+    var mondayPercent: Double = 0
+    var tuesdayPercent: Double = 0
+    var wednesdayPercent: Double = 0
+    var thursdayPercent: Double = 0
+    var fridayPercent: Double = 0
+    var saturdayPercent: Double = 0
+    var timeFrameText: String = ""
+    var hoursText: String = ""
+
+    init(_ weeklyObject: WeeklyObject) {
+        setupPercentData(weeklyObject.weekDaysWorkingPercentage)
+        if let hourString = weeklyObject.totalWorkTime.convert(preserving: [.hour]) {
+            if let hourInt = Int(hourString) {
+                if hourInt < 2 {
+                    hoursText = "\(hourInt) hour"
+                } else {
+                    hoursText = "\(hourInt) hours"
+                }
+            }
+        }
+        if let weekAndTheYear = weeklyObject.weekAndTheYear {
+            timeFrameText = weekAndTheYear
+        }
+    }
+
+    func setupPercentData(_ weekDaysWorkingPercentage: WeekDaysWorkingPercent) {
+        sundayPercent = weekDaysWorkingPercentage.sundayPercent
+        mondayPercent = weekDaysWorkingPercentage.mondayPercent
+        tuesdayPercent = weekDaysWorkingPercentage.tuesdayPercent
+        wednesdayPercent = weekDaysWorkingPercentage.wednesdayPercent
+        thursdayPercent = weekDaysWorkingPercentage.thursdayPercent
+        fridayPercent = weekDaysWorkingPercentage.fridayPercent
+        saturdayPercent = weekDaysWorkingPercentage.saturdayPercent
+    }
+
 }
 
